f81449e07764d2d09ad33ee583544d3dcf9307b3
diego moreno <diegosvm@hotmail.com>
2018-12-27 21:01:51 -0200

    first commit

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  Reactotron
+    .configure()
+    .useReactNative()
+    .connect()
+    .clear();
+}
+
+console.tron = Reactotron;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import 'config/ReactotronConfig';
+const App = () => <View />
+
+export default App;
