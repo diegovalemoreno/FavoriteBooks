1f25472c6339e9aee54aea6b8b40380cdf18fe78
diego moreno <diegosvm@hotmail.com>
2018-12-27 23:46:39 -0200

    Estilizacao da pagina Main.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import {
+  View,
+  StatusBar,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import styles from './styles';
 
 const Main = () => (
-  <View style={styles.container}>
+  <SafeAreaView style={styles.container}>
     <StatusBar barStyle="light-content" />
-  </View>
+    <View style={styles.content}>
+      <Text style={styles.title}>Booksmark</Text>
+      <Text style={styles.description}>
+      Comece adicionando os seus livros aos favoritos
+      </Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="título do livro/ISBN"
+          underlineColorAndroid="transparent"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {}}
+          activeOpacity={0.6}
+        >
+          <Text style={styles.buttonText}>Adicionar aos favoritos</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    <View style={styles.footer}>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style={styles.footerLink}>
+              Meus livros favoritos (3)
+        </Text>
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
 );
 
 Main.navigationOptions = {
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,10 +1,81 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '../../styles';
+import { colors, metrics } from '../../styles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
+    alignItems: 'center',
+  },
+
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: metrics.basePadding * 2,
+  },
+
+  title: {
+    fontSize: 32,
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+
+  description: {
+    fontSize: 14,
+    color: colors.white,
+    lineHeight: 20,
+    marginTop: metrics.baseMargin / 2,
+    textAlign: 'center',
+  },
+
+  form: {
+    marginTop: metrics.baseMargin * 2,
+    alignSelf: 'stretch',
+  },
+
+  error: {
+    fontWeight: 'bold',
+    color: colors.danger,
+    alignSelf: 'center',
+    marginBottom: metrics.baseMargin,
+  },
+
+  input: {
+    height: 50,
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+  },
+
+  button: {
+    height: 50,
+    backgroundColor: colors.secundary,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: metrics.baseMargin,
+  },
+
+  buttonText: {
+    color: colors.darkTransparent,
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+
+  loading: {
+    color: colors.darkTransparent,
+  },
+
+  footer: {
+    paddingBottom: metrics.basePadding,
+  },
+
+  footerLink: {
+    fontSize: 14,
+    color: colors.white,
+    fontWeight: 'bold',
   },
 });
 
